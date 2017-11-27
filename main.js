921e4e3bc1da3852d1b2e6afaa4219f465debbd2
Brala <elobralu@gmail.com>
2017-11-27 18:43:15 +0100

    grand changes

--- main.js
+++
@@ -1,6 +0,0 @@
-var menu = document.querySelector(".logo");
-menu.addEventListener("click", function () {
-    document.querySelector(".section").classList.toggle("opened");
-    document.querySelector(".main").classList.toggle("opened");
-    
-}, false);
